57b7ac7d65507c2d4b797faa398e183ef6a56046
Bobby Dean <bobby@deanhome.net>
2012-06-20 07:53:33 -0500

    Site Updated

--- modules/overlay/overlay-parent.js
+++ modules/overlay/overlay-parent.js
@@ -1,4 +1,3 @@
-
 (function ($) {
 
 /**
@@ -334,7 +333,7 @@ Drupal.overlay.setFocusBefore = function ($element, document) {
   $placeholder.one('blur', function () {
     $(this).remove();
   });
-}
+};
 
 /**
  * Check if the given link is in the administrative section of the site.
@@ -346,13 +345,22 @@ Drupal.overlay.setFocusBefore = function ($element, document) {
  *   TRUE if the URL represents an administrative link, FALSE otherwise.
  */
 Drupal.overlay.isAdminLink = function (url) {
+  if (Drupal.overlay.isExternalLink(url)) {
+    return false;
+  }
+
   var path = this.getPath(url);
 
   // Turn the list of administrative paths into a regular expression.
   if (!this.adminPathRegExp) {
-    var regExpPrefix = '^' + Drupal.settings.pathPrefix + '(';
-    var adminPaths = regExpPrefix + Drupal.settings.overlay.paths.admin.replace(/\s+/g, ')$|' + regExpPrefix) + ')$';
-    var nonAdminPaths = regExpPrefix + Drupal.settings.overlay.paths.non_admin.replace(/\s+/g, ')$|'+ regExpPrefix) + ')$';
+    var prefix = '';
+    if (Drupal.settings.overlay.pathPrefixes.length) {
+      // Allow path prefixes used for language negatiation followed by slash,
+      // and the empty string.
+      prefix = '(' + Drupal.settings.overlay.pathPrefixes.join('/|') + '/|)';
+    }
+    var adminPaths = '^' + prefix + '(' + Drupal.settings.overlay.paths.admin.replace(/\s+/g, '|') + ')$';
+    var nonAdminPaths = '^' + prefix + '(' + Drupal.settings.overlay.paths.non_admin.replace(/\s+/g, '|') + ')$';
     adminPaths = adminPaths.replace(/\*/g, '.*');
     nonAdminPaths = nonAdminPaths.replace(/\*/g, '.*');
     this.adminPathRegExp = new RegExp(adminPaths);
@@ -360,6 +368,20 @@ Drupal.overlay.isAdminLink = function (url) {
   }
 
   return this.adminPathRegExp.exec(path) && !this.nonAdminPathRegExp.exec(path);
+};
+
+/**
+ * Determine whether a link is external to the site.
+ *
+ * @param url
+ *   The url to be tested.
+ *
+ * @return boolean
+ *   TRUE if the URL is external to the site, FALSE otherwise.
+ */
+Drupal.overlay.isExternalLink = function (url) {
+  var re = RegExp('^((f|ht)tps?:)?//(?!' + window.location.host + ')');
+  return re.test(url);
 };
 
 /**
@@ -413,6 +435,27 @@ Drupal.overlay.eventhandlerAlterDisplacedElements = function (event) {
   // IE6 doesn't support maxWidth, use width instead.
   var maxWidthName = (typeof document.body.style.maxWidth == 'string') ? 'maxWidth' : 'width';
 
+  if (Drupal.overlay.leftSidedScrollbarOffset === undefined && $(document.documentElement).attr('dir') === 'rtl') {
+    // We can't use element.clientLeft to detect whether scrollbars are placed
+    // on the left side of the element when direction is set to "rtl" as most
+    // browsers dont't support it correctly.
+    // http://www.gtalbot.org/BugzillaSection/DocumentAllDHTMLproperties.html
+    // There seems to be absolutely no way to detect whether the scrollbar
+    // is on the left side in Opera; always expect scrollbar to be on the left.
+    if ($.browser.opera) {
+      Drupal.overlay.leftSidedScrollbarOffset = document.documentElement.clientWidth - this.iframeWindow.document.documentElement.clientWidth + this.iframeWindow.document.documentElement.clientLeft;
+    }
+    else if (this.iframeWindow.document.documentElement.clientLeft) {
+      Drupal.overlay.leftSidedScrollbarOffset = this.iframeWindow.document.documentElement.clientLeft;
+    }
+    else {
+      var el1 = $('<div style="direction: rtl; overflow: scroll;"></div>').appendTo(document.body);
+      var el2 = $('<div></div>').appendTo(el1);
+      Drupal.overlay.leftSidedScrollbarOffset = parseInt(el2[0].offsetLeft - el1[0].offsetLeft);
+      el1.remove();
+    }
+  }
+
   // Consider any element that should be visible above the overlay (such as
   // a toolbar).
   $('.overlay-displace-top, .overlay-displace-bottom').each(function () {
@@ -421,6 +464,10 @@ Drupal.overlay.eventhandlerAlterDisplacedElements = function (event) {
     // In IE, Shadow filter makes element to overlap the scrollbar with 1px.
     if (this.filters && this.filters.length && this.filters.item('DXImageTransform.Microsoft.Shadow')) {
       maxWidth -= 1;
+    }
+
+    if (Drupal.overlay.leftSidedScrollbarOffset) {
+      $(this).css('left', Drupal.overlay.leftSidedScrollbarOffset);
     }
 
     // Prevent displaced elements overlapping window's scrollbar.
@@ -435,7 +482,12 @@ Drupal.overlay.eventhandlerAlterDisplacedElements = function (event) {
     var offset = $(this).offset();
     var offsetRight = offset.left + $(this).outerWidth();
     if ((data.drupalOverlay && data.drupalOverlay.clip) || offsetRight > maxWidth) {
-      $(this).css('clip', 'rect(auto, ' + (maxWidth - offset.left) + 'px, ' + (documentHeight - offset.top) + 'px, auto)');
+      if (Drupal.overlay.leftSidedScrollbarOffset) {
+        $(this).css('clip', 'rect(auto, auto, ' + (documentHeight - offset.top) + 'px, ' + (Drupal.overlay.leftSidedScrollbarOffset + 2) + 'px)');
+      }
+      else {
+        $(this).css('clip', 'rect(auto, ' + (maxWidth - offset.left) + 'px, ' + (documentHeight - offset.top) + 'px, auto)');
+      }
       (data.drupalOverlay = data.drupalOverlay || {}).clip = true;
     }
   });
@@ -808,8 +860,13 @@ Drupal.overlay.getDisplacement = function (region) {
   if (lastDisplaced.length) {
     displacement = lastDisplaced.offset().top + lastDisplaced.outerHeight();
 
-    // Remove height added by IE Shadow filter.
-    if (lastDisplaced[0].filters && lastDisplaced[0].filters.length && lastDisplaced[0].filters.item('DXImageTransform.Microsoft.Shadow')) {
+    // In modern browsers (including IE9), when box-shadow is defined, use the
+    // normal height.
+    var cssBoxShadowValue = lastDisplaced.css('box-shadow');
+    var boxShadow = (typeof cssBoxShadowValue !== 'undefined' && cssBoxShadowValue !== 'none');
+    // In IE8 and below, we use the shadow filter to apply box-shadow styles to
+    // the toolbar. It adds some extra height that we need to remove.
+    if (!boxShadow && /DXImageTransform\.Microsoft\.Shadow/.test(lastDisplaced.css('filter'))) {
       displacement -= lastDisplaced[0].filters.item('DXImageTransform.Microsoft.Shadow').strength;
       displacement = Math.max(0, displacement);
     }
@@ -913,7 +970,7 @@ Drupal.overlay._recordTabindex = function () {
   var $element = $(this);
   var tabindex = $(this).attr('tabindex');
   $element.data('drupalOverlayOriginalTabIndex', tabindex);
-}
+};
 
 /**
  * Restore an element's original tabindex.
--- sites/all/modules/context/plugins/context_reaction_block.js
+++ sites/all/modules/context/plugins/context_reaction_block.js
@@ -173,10 +173,13 @@ DrupalContextBlockEditor.prototype.initBlocks = function(blocks) {
   var self = this;
   this.blocks = blocks;
   blocks.each(function() {
+    if($(this).hasClass('context-block-empty')) {
+      $(this).removeClass('context-block-hidden');
+    }
     $(this).addClass('draggable');
     $(this).prepend($('<a class="context-block-handle"></a>'));
     $(this).prepend($('<a class="context-block-remove"></a>').click(function() {
-      $(this).parents('div.block').eq(0).fadeOut('medium', function() {
+      $(this).parent ('.block').eq(0).fadeOut('medium', function() {
         $(this).remove();
         self.updateBlocks();
       });
@@ -196,7 +199,7 @@ DrupalContextBlockEditor.prototype.updateBlocks = function() {
   var browser = $('div.context-block-browser');
 
   // For all enabled blocks, mark corresponding addables as having been added.
-  $('div.block, div.admin-block').each(function() {
+  $('.block, .admin-block').each(function() {
     var bid = $(this).attr('id').split('block-')[1]; // Ugh.
     $('#context-block-addable-'+bid, browser).draggable('disable').addClass('context-block-added').removeClass('context-block-addable');
   });
@@ -210,7 +213,7 @@ DrupalContextBlockEditor.prototype.updateBlocks = function() {
 
   // Mark empty regions.
   $(this.regions).each(function() {
-    if ($('div.block:has(a.context-block)', this).size() > 0) {
+    if ($('.block:has(a.context-block)', this).size() > 0) {
       $(this).removeClass('context-block-region-empty');
     }
     else {
@@ -230,8 +233,8 @@ DrupalContextBlockEditor.prototype.updateRegion = function(event, ui, region, op
     case 'out':
       if (
         // jQuery UI 1.8
-        $('div.draggable-placeholder', region).size() === 1 &&
-        $('div.block:has(a.context-block)', region).size() == 0
+        $('.draggable-placeholder', region).size() === 1 &&
+        $('.block:has(a.context-block)', region).size() == 0
         // jQuery UI 1.6
         // $('div.draggable-placeholder', region).size() === 0 &&
         // $('div.block:has(a.context-block)', region).size() == 1 &&
@@ -326,13 +329,13 @@ DrupalContextBlockEditor.prototype.setState = function() {
   */
 DrupalContextBlockEditor.prototype.disableTextSelect = function() {
   if ($.browser.safari) {
-    $('div.block:has(a.context-block):not(:has(input,textarea))').css('WebkitUserSelect','none');
+    $('.block:has(a.context-block):not(:has(input,textarea))').css('WebkitUserSelect','none');
   }
   else if ($.browser.mozilla) {
-    $('div.block:has(a.context-block):not(:has(input,textarea))').css('MozUserSelect','none');
+    $('.block:has(a.context-block):not(:has(input,textarea))').css('MozUserSelect','none');
   }
   else if ($.browser.msie) {
-    $('div.block:has(a.context-block):not(:has(input,textarea))').bind('selectstart.contextBlockEditor', function() { return false; });
+    $('.block:has(a.context-block):not(:has(input,textarea))').bind('selectstart.contextBlockEditor', function() { return false; });
   }
   else {
     $(this).bind('mousedown.contextBlockEditor', function() { return false; });
@@ -370,7 +373,7 @@ DrupalContextBlockEditor.prototype.editStart = function(editor, context) {
   this.editor.addClass('context-editing');
 
   this.disableTextSelect();
-  this.initBlocks($('div.block:has(a.context-block.edit-'+context+')'));
+  this.initBlocks($('.block:has(a.context-block.edit-'+context+')'));
   this.initRegions($('a.context-block-region').parent());
   this.updateBlocks();
 
@@ -383,7 +386,7 @@ DrupalContextBlockEditor.prototype.editStart = function(editor, context) {
       dropOnEmpty: true,
       placeholder: 'draggable-placeholder',
       forcePlaceholderSize: true,
-      items: '> div.block:has(a.context-block.editable)',
+      items: '> .block:has(a.context-block.editable)',
       handle: 'a.context-block-handle',
       start: function(event, ui) { self.scriptFix(event, ui, editor, context); },
       stop: function(event, ui) { self.addBlock(event, ui, editor, context); },
@@ -419,6 +422,9 @@ DrupalContextBlockEditor.prototype.editFinish = function() {
   // Remove UI elements.
   $(this.blocks).each(function() {
     $('a.context-block-handle, a.context-block-remove', this).remove();
+    if($(this).hasClass('context-block-empty')) {
+      $(this).addClass('context-block-hidden');
+    }
     $(this).removeClass('draggable');
   });
   this.regions.sortable('destroy');
